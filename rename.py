efbe467633638677029a4ec5168b4f4b46a5e35b
Lr_2002 <2629651228@qq.com>
2025-05-06 14:15:27 +0800

    feat: rename py

--- rename.py
+++ rename.py
@@ -1,6 +1,12 @@
 import os
 
-media = os.listdir("./medias/")
-for name in media:
-    print(name)
-    os.rename("./medias/" + name, "./medias/" + name.replace("-", "_"))
+
+def process(dirs):
+    media = os.listdir(dirs)
+    for name in media:
+        print(name)
+        os.rename(os.path.join(dirs, name), os.path.join(dirs, name.replace("-", "_")))
+
+
+process("medias")
+process("interactive_task_image")
